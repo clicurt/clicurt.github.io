ee88cdc9fee7de194230d0812c63efbc3ab35bd8
clicurt <clicurt@gmail.com>
2016-07-14 05:34:12 +0900

    welcome

--- clicurt-site/welcome_get.php
+++ clicurt-site/welcome_get.php
@@ -1,15 +1,4 @@
-<!doctype html>
-<html lang="en">
- <head>
-  <meta charset="UTF-8">
-  <meta name="Generator" content="EditPlus®">
-  <meta name="Author" content="">
-  <meta name="Keywords" content="">
-  <meta name="Description" content="">
-  <title>Welcome Get</title>
- </head>
- <body>
+<?php
   Welcome <?php echo $_GET['name']; ?><br>
   Your email address is <?php echo $_GET["email"]; ?>
- </body>
-</html>
+?>
---
+++ clicurt-site/welcome_get.php.bak
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="Generator" content="EditPlus®">
+  <meta name="Author" content="">
+  <meta name="Keywords" content="">
+  <meta name="Description" content="">
+  <title>Welcome Get</title>
+ </head>
+ <body>
+  Welcome <?php echo $_GET['name']; ?><br>
+  Your email address is <?php echo $_GET["email"]; ?>
+ </body>
+</html>
